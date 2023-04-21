d3da7da4fb597bf64095ff4a3a05859b63b2ffa6
Enzo <enzo.velarde@tecsup.edu.pe>
2023-04-20 20:37:58 -0500

    C

--- App.js
+++ App.js
@@ -1,19 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Grid, makeStyles, Box, withWidth } from '@material-ui/core';
 
-export default function App() {
+const estilos = makeStyles((theme) => ({
+  fondo: {
+    background: theme.palette.secondary,
+  },
+}));
+
+const Columnas = (props) => {
+  const classes = estilos();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6} lg={3} className={classes.fondo}>
+          <Box
+            bgcolor='primary.main'
+            color='primary.contrastText'
+            p={2}
+            textAlign='center'
+          >
+            {props.width}
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} className={classes.fondo}>
+          <Box
+            bgcolor='primary.main'
+            color='primary.contrastText'
+            p={2}
+            textAlign='center'
+          >
+            {props.width}
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} className={classes.fondo}>
+          <Box
+            bgcolor='secondary.main'
+            color='secondary.contrastText'
+            p={2}
+            textAlign='center'
+          >
+            {props.width}
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} className={classes.fondo}>
+          <Box
+            bgcolor='secondary.main'
+            color='secondary.contrastText'
+            p={2}
+            textAlign='center'
+          >
+            {props.width}
+          </Box>
+        </Grid>
+      </Grid>
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Columnas></Columnas>
+    </View>
+  );
+};
+
+export default withWidth()(App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
